b981f9165ad05b3086814ca88ff8b14897918ddc
Juan Dematei <juandematei@gmail.com>
2018-09-25 21:00:07 -0300

    media queries

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,5 +1,3 @@
-AOS.init();
-
 // Select DOM elements
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
@@ -12,11 +10,13 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
   if (!showMenu) {
     menu.classList.add('show');
-
+    
     showMenu = true;
   } else {
     menu.classList.remove('show');
-
+    
     showMenu = false;
   }
 }
+
+AOS.init();
