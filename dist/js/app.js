c30d4a22d828668bc66f58d3a3cf11b092919ca4
Juan Dematei <juandematei@gmail.com>
2018-09-26 16:57:29 -0300

    side menu hides after link click

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,31 +1,35 @@
 // Select DOM elements
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
-const navLink = document.querySelector('.nav-link');
 
 // Menu initial state
 let showMenu = false;
 
 // Toggle function
 menuBtn.addEventListener('click', toggleMenu);
-navLink.addEventListener('click', toggleMenuLink);
 
 function toggleMenu() {
   if (!showMenu) {
     menu.classList.add('show');
-    
     showMenu = true;
 
   } else {
     menu.classList.remove('show');
-    
     showMenu = false;
   }
 }
 
-function toggleMenuLink() {
-    menu.classList.remove('show');
-    showMenu = false;
+// Link click function
+var navLink = document.querySelectorAll('.nav-link'), result;
+for (var i = 0; i < navLink.length; i++) {
+  result = navLink[i];
+  result.addEventListener('click', closeMenu);
+}
+
+function closeMenu() {
+  //console.log('Hola');
+  menu.classList.remove('show');
+  showMenu = false;
 }
 
 // AOS animations
